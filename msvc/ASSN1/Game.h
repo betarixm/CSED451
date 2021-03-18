9be0b08637ae82c4de0fae6c6c25e6857058081d
beta-lux <beta.underalpha@gmail.com>
2021-03-18 23:19:25 +0900

    msvc-ed

---
+++ msvc/ASSN1/Game.h
@@ -0,0 +1,47 @@
+#ifndef CSED451_GAME_H
+#define CSED451_GAME_H
+
+#include "Ship.h"
+
+#define PLAYER_DEGREE 180.0
+#define ENEMY_DEGREE 0.0
+
+#define NUM_PLAYER 1
+#define NUM_ENEMY 5
+#define LIFE_PLAYER 3
+
+class Game {
+private:
+    int numPlayer = NUM_PLAYER, numEnemy = NUM_ENEMY;
+    int numLifePlayer = LIFE_PLAYER, numLifeEnemy = 1;
+    bool isPlayerDead = true, isEnemyDead = true, _isGameOver = false, _isGameWin = false;
+    Player* _player = nullptr;
+    Enemy* _enemy = nullptr;
+
+protected:
+public:
+    Game();
+
+    ~Game();
+
+    void spawnPlayer();
+
+    void spawnEnemy();
+
+    void tick();
+
+    void displayInfo();
+
+    void display();
+
+    Player* player() const;
+
+    Enemy* enemy() const;
+
+    bool isGameOver() const;;
+
+    bool isGameWin() const;
+};
+
+
+#endif //CSED451_GAME_H
---
+++ msvc/ASSN1/Shape.h
@@ -0,0 +1,75 @@
+#ifndef CSED451_ASSN1_SHAPE_H
+#define CSED451_ASSN1_SHAPE_H
+
+#include <GL/glew.h>
+#include <vector>
+
+#define NUM_COLOR 3
+
+#define R 0
+#define G 1
+#define B 2
+
+#define SQRT_3 1.732f
+
+class Shape {
+private:
+    float _x, _y, _length, _degree = 0;
+    GLclampf _colorfv[3]{};
+
+protected:
+    void _display(float rotateDeg, std::vector<std::vector<float>> &mat, GLenum mode);
+
+public:
+    Shape(float x, float y, float length, GLclampf r, GLclampf g, GLclampf b);
+
+    Shape(float x, float y, float length, const GLclampf colorfv[]);
+
+    void move(float dx, float dy);
+
+    void rotate(float ddeg);
+
+    void setDegree(float deg);
+
+    void mutateColor(GLclampf dr, GLclampf dg, GLclampf db) {
+        this->_colorfv[R] += dr;
+        this->_colorfv[G] += dg;
+        this->_colorfv[B] += db;
+    }
+
+    void mutateColor(const GLclampf dColorfv[]);
+
+    float x() const;
+
+    float y() const;
+
+    float length() const;
+
+    GLclampf *color();
+
+    float degree() const;
+};
+
+class Triangle : public Shape {
+public:
+    Triangle(float x, float y, float length, GLclampf r, GLclampf g, GLclampf b);
+
+    Triangle(float x, float y, float length, const GLclampf colorfv[]);
+
+    void display();
+
+    void display(float rotateDeg);
+};
+
+class Square : public Shape {
+public:
+    Square(float x, float y, float length, GLclampf r, GLclampf g, GLclampf b);
+
+    Square(float x, float y, float length, const GLclampf colorfv[]);
+
+    void display();
+
+    void display(float rotateDeg);
+};
+
+#endif // CSED451_ASSN1_SHAPE_H
