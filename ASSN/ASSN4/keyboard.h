9ae14355d222fcd2a5a7020a4d4b3a857306c5de
beta-lux <beta.underalpha@gmail.com>
2021-05-13 17:00:02 +0900

    ifndef patch

--- ASSN/ASSN4/keyboard.h
+++ ASSN/ASSN4/keyboard.h
@@ -1,3 +1,6 @@
+#ifndef CSED451_KEYBOARD_H
+#define CSED451_KEYBOARD_H
+
 #include "Game.h"
 
 extern Game *game;
@@ -12,3 +15,5 @@ void onSpecialKeyUp(int key, int x, int y);
 void onKeyDown(unsigned char key, int x, int y);
 
 void onKeyUp(unsigned char key, int x, int y);
+
+#endif //CSED451_KEYBOARD_H
