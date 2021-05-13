fd19740ff715ebec90b4bbffa0879e2a54daa12b
beta-lux <beta.underalpha@gmail.com>
2021-05-13 17:05:18 +0900

    Timer functions split

---
+++ ASSN/ASSN4/timer.h
@@ -0,0 +1,28 @@
+#ifndef CSED451_TIMER_H
+#define CSED451_TIMER_H
+
+#include "Game.h"
+#include "Ship.h"
+#include "Stellar.h"
+
+extern Game *game;
+
+extern list<Bullet *> enemy_bullets;
+extern list<Bullet *> player_bullets;
+extern list<Item *> item_list;
+
+extern vector<Stellar *> stellar_vec;
+
+extern char mode;
+
+void timerBulletMoveHit(int value);
+
+void timerBulletEnemyShot(int value);
+
+void timerDefault(int value);
+
+void timerRedisplay(int value);
+
+void timerStellar(int value);
+
+#endif //CSED451_TIMER_H
