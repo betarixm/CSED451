0b88aac27ce8b7b2a9df7213ebef51dc6465c765
beta-lux <beta.underalpha@gmail.com>
2021-05-13 17:05:13 +0900

    Timer functions split

--- ASSN/ASSN4/main.cpp
+++ ASSN/ASSN4/main.cpp
@@ -8,15 +8,17 @@
 #include "Shader.h"
 #include "Stellar.h"
 #include "Ship.h"
+
 #include "keyboard.h"
+#include "timer.h"
 
 
 char mode = 'n'; /* n : normal,  f: fail, c: king-god */
 bool isHiddenLineRemoval = false;
 int frontCamera = -1;
-static list<Bullet*> enemy_bullets;
+list<Bullet*> enemy_bullets;
 list<Bullet*> player_bullets;
-static list<Item*> item_list;
+list<Item*> item_list;
 
 vector<Stellar*> stellar_vec;
 
@@ -42,7 +44,6 @@ void lookAt(float x, float y, int _frontCamera) {
     glm::mat4 cur_C = ModelView.back();
     ModelView.pop_back();
     ModelView.push_back(C*cur_C);
-
 }
 
 void microRenderScene(bool isBlack) {
@@ -92,137 +93,6 @@ void renderScene() {
     }
 
     glutSwapBuffers();
-}
-
-/**
- * @brief 일정초마다 총알 이동 및 충돌 체크
- */
-void timerBulletMoveHit(int value)
-{
-    list<Bullet*>::iterator itr;
-    Bullet* bullet;
-    vector<float> dir;
-    float x, y;
-
-    itr = enemy_bullets.begin();
-    while(itr != enemy_bullets.end())
-    {
-        bullet = *itr;
-        dir = bullet->direction();
-        bullet->move(-1*dir[0], -1*dir[1]);  /** window 밖으로 나가는 것 방지 */
-        x = bullet->x();
-        y = bullet->y();
-
-        if ((x > 1.0) || (x < -1.0))
-        {
-            bullet->setDirection(-1*dir[0], dir[1], 0);
-        }
-        if(y < -1.0)
-        {
-            enemy_bullets.erase(itr++);
-            delete(bullet);
-        }
-        else
-            ++itr;
-    }
-
-    itr = player_bullets.begin();
-    while(itr != player_bullets.end())
-    {
-        bullet = *itr;
-        bullet->move(0, +0.1);
-
-        dir = bullet->direction();
-        bullet->move(dir[0], dir[1]);  /** window 밖으로 나가는 것 방지 */
-        x = bullet->x();
-        y = bullet->y();
-
-        if ((x > 1.0) || (x < -1.0))
-        {
-            bullet->setDirection(-1*dir[0], dir[1], 0);
-        }
-
-        if(y > 1.0)           /** window 밖으로 나가는 것 방지 */
-        {
-            player_bullets.erase(itr++);
-            delete(bullet);
-        }
-        else
-            ++itr;
-    }
-
-
-    if(game->player()){
-        game->player()->HitBullet(&enemy_bullets);
-        game->player()->HitItem(&item_list);
-    }
-
-    if(game->enemy()){
-        game->enemy()->checkHit(&player_bullets);
-    }
-
-    glutTimerFunc(30, timerBulletMoveHit, 1);
-}
-/**
- * @brief 일정초마다 _enemy moving & 총알 발
- * @param value 7번에 1번씩 총알 발사를 위한 counter
- */
-void timerBulletEnemyShot(int value)
-{
-    list<Bullet*>::iterator itr;
-
-    if(game->enemy()){
-        list<Bullet*> new_bullet;
-
-        game->enemy()->randomMoveHandler();
-
-        if(value == 0) {    /* 총알 발사 */
-            new_bullet = game->enemy()->shot();
-            for(Bullet* ptr : new_bullet)
-                enemy_bullets.push_back(ptr);
-        }
-
-    }
-
-    Item *item;
-    itr = item_list.begin();
-    while(itr != item_list.end())
-    {
-        item = *itr;
-        item->move(0, -0.1);
-        if((*itr)->y() < -1.0)           /** window 밖으로 나가는 것 방지 */
-        {
-            item_list.erase(itr++);
-            delete(item);
-        }
-        else
-            ++itr;
-    }
-
-    glutTimerFunc(300, timerBulletEnemyShot, (value + 1) % 7);
-    if(value==-1)
-        glutTimerFunc(30, timerBulletMoveHit, 1);
-
-}
-
-void timerDefault(int value){
-    Item *new_item = game->tick();
-    if (new_item != nullptr)
-        item_list.push_back(new_item);
-
-    glutTimerFunc(1, timerDefault, -1);
-}
-
-void timerRedisplay(int value) {
-    glutPostRedisplay();
-    glutTimerFunc(1, timerRedisplay, -1);
-}
-
-void timerStellar(int value) {
-    for(auto & i : stellar_vec){
-        i->tick();
-    }
-    glutTimerFunc(1, timerStellar, -1);
 }
 
 void initGraphic()
